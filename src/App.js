13cc3cc456ec4982e14b1de4f6ecf1cb67c1cdce
PadmajaLakum <73012653+PadmajaLakum@users.noreply.github.com>
2021-05-10 08:18:22 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, {useState,useEffect} from "react";
 import Recipe from './Recipe.';
 import './style.css';
 
-
-
 const App = () => {
 
   const APP_ID="1230be33";
@@ -13,6 +11,7 @@ const App = () => {
   const [search,setSearch]=useState('');
   const [query,setQuery]=useState('cheesecake');
   useEffect(() => { getRecipes();},[query]);  //eslint-disable-line 
+  // getting API information from EDAMAM 
   const getRecipes= async () => {
     const response= await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data=await response.json();
@@ -20,8 +19,6 @@ const App = () => {
     console.log(data.hits);
     
   }
-
-
 const updateSearch= e => {
   setSearch(e.target.value);
 }
@@ -30,16 +27,13 @@ const getSearch =e => {
   setQuery(search);
   setSearch('');
 }
- 
-
 return(
   <div className="App">
     <form className="search-form" onSubmit={getSearch}>
       <input type="text" className="search-bar"  value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit"> search</button>
         </form>
-      
-          <div className='recipes'>
+           <div className='recipes'>
             {recipes.map(recipe => (
               <Recipe key={recipe.recipe.label}
               title={recipe.recipe.label}
@@ -50,15 +44,10 @@ return(
             )
           }
           </div>
-        
-
-    
-  </div>
+   
+     </div>
 )
         }
-
-
-
 
 export default App;
 
